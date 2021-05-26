25f2f80ceeb8ac14357af9600faed7c18f8e2ffb
Pixel <1049255321@qq.com>
2021-05-26 17:09:54 +0800

    更2

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -1,5 +1,8 @@
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello 更新!");
+        System.out.println("Hello Git!");
+System.out.println("7f6c0d63fb422213439e205380d3a223f7e74cc009");
     }
 }
