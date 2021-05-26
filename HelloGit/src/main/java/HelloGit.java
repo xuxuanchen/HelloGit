5e3effc54908e7f2e1b1c1826a225ec917ebca13
Pixel <1049255321@qq.com>
2021-05-26 18:39:55 +0800

    创建分支

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -3,6 +3,6 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("Hello 更新!");
         System.out.println("Hello Git!");
-System.out.println("7f6c0d63fb422213439e205380d3a223f7e74cc009");
+System.out.println("创建分支");
     }
 }
